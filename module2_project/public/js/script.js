7fed2c5afa4855d2da6b18b76b16a3362842db18
Mariana Silva <maryana_silva@sapo.pt>
2023-08-11 16:57:35 +0100

    added initial config

---
+++ module2_project/public/js/script.js
@@ -0,0 +1,4 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("module2_project JS imported successfully!");
+});
