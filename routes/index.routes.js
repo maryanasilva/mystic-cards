8ae46151c45fa63f1d43a52d8e3f8cd2243093df
Mariana Silva <maryana_silva@sapo.pt>
2023-08-16 15:35:52 +0100

    cards && reviews && deletes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
+  /*   const user = req.session.currentUser;
+  console.log(user); */
   res.render("index");
+});
+
+/* GET How to Play Page */
+router.get("/how-to-play", (req, res, next) => {
+  /*   const user = req.session.currentUser;
+  console.log(user); */
+  res.render("how-to-play");
 });
 
 module.exports = router;
